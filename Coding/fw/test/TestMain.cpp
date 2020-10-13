7df93492a53512bf3fb8d6fb30961c02442ca68e
Reyes Gabryel <Gabryel.Reyes@newtec.de>
2020-10-13 10:28:25 +0200

    Setup of Native Test Environment

---
+++ Coding/fw/test/TestMain.cpp
@@ -0,0 +1,31 @@
+/***************************************************************************************************
+  (c) NewTec GmbH 2020   -   www.newtec.de
+  $URL: https://github.com/NewTec-GmbH/esp32-can-iot $
+***************************************************************************************************/
+/**
+@addtogroup Testing
+@{
+@file       TestMain.h
+
+Main Test entry point
+
+* @}
+***************************************************************************************************/
+/* INCLUDES ***************************************************************************************/
+#include <unity.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <Lawicel.h>
+#include <Arduino.h>
+#include <Print.h>
+#include <WString.h>
+#include <math.h>
+#include <cmath>
+
+int main(int argc, char **argv)
+{
+    UNITY_BEGIN();    
+
+    return UNITY_END();
+}
