bf21cd884f98cd1d715d963bf84394e206e31af9
Reyes Gabryel <Gabryel.Reyes@newtec.de>
2020-10-29 15:50:12 +0100

    Defines methods on Settings.h

--- Coding/fw/lib/Settings/Settings.h
+++ Coding/fw/lib/Settings/Settings.h
@@ -15,6 +15,7 @@ Handler for ESP32 Preferences in Flash Memory
 #define SETTINGS_H_
 
 /* INCLUDES ***************************************************************************************/
+#include <Preferences.h>
 
 /* C-Interface ************************************************************************************/
 extern "C"
@@ -22,8 +23,29 @@ extern "C"
 }
 
 /* FORWARD DECLARATIONS ***************************************************************************/
+/*
+* Implementation of the Preferences Library for the project. Initial values and commands are
+* saved and called through this class.
+*/
 class Settings
 {
+public:
+    Settings()
+    {
+    }
+
+    ~Settings()
+    {
+    }
+
+    bool save(const char *directory, const String &key, const uint32_t &value);     /**< Saves uint32_t value to Flash memory on location "key" */
+    bool save(const char *directory, const String &key, const String &value);       /**< Saves String to Flash memory on location "key" */
+    bool get(const char *directory, const String &key, uint32_t &value);            /**< Gets uint32_t value from Flash memory on location "key". Writes it in "value" */
+    bool get(const char *directory, const String &key, String &value);              /**< Gets String from Flash memory on location "key". Writes it in "value" */
+    bool clear(const char *directory);                                              /**< Clears all entries on a directory */
+
+private:
+    Preferences m_memory;
 };
 
 /* INLINE FUNCTIONS ***************************************************************************/
