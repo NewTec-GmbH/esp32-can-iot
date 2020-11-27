719363d0eb9740f3e61f78afd57e45d25490c801
Reyes Gabryel <Gabryel.Reyes@newtec.de>
2020-11-27 17:17:44 +0100

    Websocket Adapter for Lawicel implemented

---
+++ Coding/fw/src/Adapters/WSAdapter.h
@@ -0,0 +1,130 @@
+/***************************************************************************************************
+  (c) NewTec GmbH 2020   -   www.newtec.de
+  $URL: https://github.com/NewTec-GmbH/esp32-can-iot $
+***************************************************************************************************/
+/**
+@addtogroup Adapters
+@{
+@file       WSAdapter.h
+
+ESP32 WebSocket Adapter for Lawicel Protocol
+
+* @}
+***************************************************************************************************/
+#ifndef WS_ADAPTER_H
+#define WS_ADAPTER_H
+
+/* INCLUDES ***************************************************************************************/
+#include <Serialinterface.h>
+#include <Websocket.h>
+
+/* C-Interface ************************************************************************************/
+extern "C"
+{
+}
+
+/* FORWARD DECLARATIONS ***************************************************************************/
+
+/**
+*  ESP-32 Adapter as implementation of SerialInterface for the Lawicel Protocol.
+*/
+class WSAdapter : public SerialInterface
+{
+public:
+    /* CONSTANTS ******************************************************************************/
+
+    /* TYPES **********************************************************************************/
+
+    /**
+    *  Default constructor creates instance of the class using default values.
+    * @param m_baudrate     Sets the Default baudrate to the Serial Channel.
+    */
+    WSAdapter() : SerialInterface()
+    {
+    }
+
+    /**
+    * Default destructor deletes instance of the class.
+    */
+    ~WSAdapter()
+    {
+    }
+
+    /**
+    * Configures and starts the Serial Controller to use the user values.
+    * @return 0 for OK, 1 for Error
+    */
+    bool begin()
+    {
+        return true;
+    }
+
+    /**
+    * Stops the Serial Module without destroying the instance.
+    * @return 0 for OK, 1 for Error
+    */
+    bool end()
+    {
+        return true;
+    }
+
+    /**
+    * Sets the Baudrate for Serial Communication.
+    * @param[in] baudrate      Baudrate for Serial Communication
+    * @return 0 for OK, 1 for Error
+    */
+    bool setBaudrate(uint32_t baudrate)
+    {
+        return true;
+    }
+
+    /**
+    * Reads the Serial Adapter into a buffer.
+    * @return isError: 0 for OK, 1 for Error
+    */
+    bool read(char &c)
+    {
+        uint8_t success = true;
+        return success;
+    }
+
+    /**
+    * Prints a Line to Serial Adapter.
+    * @param[in] string     String to be printed
+    */
+    void print(const String &string)
+    {
+        websocket::send(string);
+    }
+
+    /**
+    * Prints an Integer to Serial Adapter.
+    * @param[in] num     Integer to be printed
+    */
+    void print(uint32_t num)
+    {
+        String tmp (num);
+        websocket::send(tmp);
+    }
+
+    /**
+    * Prints a Character to Serial Adapter.
+    * @param[in] c     Character to be printed
+    */
+    void print(char c)
+    {
+        String tmp (c);
+        websocket::send(tmp);
+    }
+
+protected:
+private:
+    long m_baudrate;
+    String currentCommand = "";
+};
+
+/* INLINE FUNCTIONS ***************************************************************************/
+
+/* PROTOTYPES *********************************************************************************/
+
+#endif /* SERIAL_ADAPTER_H */
--- Coding/fw/src/main.cpp
+++ Coding/fw/src/main.cpp
@@ -17,6 +17,7 @@ Main Application
 #include <SerialAdapter.h>
 #include <CANAdapter.h>
 #include <NVMAdapter.h>
+#include <WSAdapter.h>
 #include <ESP_Server.h>
 #include <Settings.h>
 #include <io.h>
@@ -34,7 +35,9 @@ Main Application
 SerialAdapter serialAdapter;
 CANAdapter sja1000Adapter;
 NVMAdapter flashAdapter;
-Lawicel protocolLawicel(serialAdapter, sja1000Adapter, flashAdapter);
+WSAdapter websocketadapter;
+//Lawicel protocolLawicel(serialAdapter, sja1000Adapter, flashAdapter);
+Lawicel protocolLawicel(websocketadapter, sja1000Adapter, flashAdapter);
 
 /* PUBLIC METHODES ********************************************************************************/
 void restart();
