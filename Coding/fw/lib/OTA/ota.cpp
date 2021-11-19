f5f320b584feffd62876bb04216724d53655cf97
Gabryel Reyes <gabryel.reyes@newtec.de>
2021-11-19 09:47:20 +0100

    Fixed Doxygen Documentation for OTA

--- Coding/fw/lib/OTA/ota.cpp
+++ Coding/fw/lib/OTA/ota.cpp
@@ -1,13 +1,56 @@
-/**************************************************************************************************
-* File: ota.cpp
-* Brief: Implementation of ota.h
-* Author: Gabryel Reyes (gabryelrdiaz@gmail.com)
-**************************************************************************************************/
+/***************************************************************************************************
+BSD 3-Clause License
+
+
+Copyright (c) 2020-2021, NewTec GmbH - www.newtec.de
+All rights reserved.
+
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+***************************************************************************************************/
+/**
+@addtogroup OTA
+@{
+@file       ota.cpp
+
+Handling of ESP32 OTA Firmware update
+
+***************************************************************************************************/
 /* INCLUDES ***************************************************************************************/
 #include "ota.h"
 #include <ArduinoOTA.h>
 
-/* CONSTANTS **************************************************************************************/
+/* C-Interface ************************************************************************************/
+extern "C"
+{
+}
 
 /* MACROS *****************************************************************************************/
 
@@ -23,10 +66,6 @@ void executeCycle(void *parameter);
 
 /**************************************************************************************************/
 
-/**
-*   Initialization of OTA
-*   @return success
-*/
 bool esp_OTA::setup()
 {
     bool success = true;
@@ -80,9 +119,10 @@ bool esp_OTA::setup()
 /**************************************************************************************************/
 
 /**
-*   Loop Handle of OTA
-*   @return success
-*/
+ *  Loop Handle of OTA
+ *  @param[in] parameter Not used. Standard definition of FreeRTOS Task.
+ *  @return success
+ */
 void executeCycle(void *parameter)
 {
     for (;;)
@@ -100,3 +140,5 @@ void executeCycle(void *parameter)
 /* EXTERNAL FUNCTIONS *****************************************************************************/
 
 /* INTERNAL FUNCTIONS *****************************************************************************/
+
+/** @} */
--- Coding/fw/lib/OTA/ota.h
+++ Coding/fw/lib/OTA/ota.h
@@ -1,29 +1,79 @@
-/**************************************************************************************************
-* File: ota.h
-* Brief: File to handle ESP32 OTA Firmware update
-* Author: Gabryel Reyes (gabryelrdiaz@gmail.com)
-**************************************************************************************************/
+/***************************************************************************************************
+BSD 3-Clause License
+
+
+Copyright (c) 2020-2021, NewTec GmbH - www.newtec.de
+All rights reserved.
+
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+***************************************************************************************************/
+/**
+@addtogroup OTA
+@{
+@file       ota.h
+
+Handling of ESP32 OTA Firmware update
+
+***************************************************************************************************/
 #ifndef OTA_H_
 #define OTA_H_
 
 /* INCLUDES ***************************************************************************************/
 
+/* C-Interface ************************************************************************************/
+extern "C"
+{
+}
+
 /* CONSTANTS **************************************************************************************/
 
-/**************************************************************************************************/
-
 /**
-*   Handler Namespace for OTA Uploads
-*/
+ *   Handler Namespace for OTA Uploads
+ */
 namespace esp_OTA
 {
-    bool setup(); /* Initialization of OTA */
-} // namespace esp_OTA
+    /** 
+     *  Initialization of OTA
+     *  @return If successfully set-up returns true. Otherwise, false.
+     */
+    bool setup(); 
+} /** namespace esp_OTA */
 
 
-/* FORWARD DECLARATIONS **************************************************************************/
+/* FORWARD DECLARATIONS ***************************************************************************/
 
-/* INLINE FUNCTIONS ******************************************************************************/
+/* INLINE FUNCTIONS ***************************************************************************/
 
-/* PROTOTYPES ************************************************************************************/
-#endif /*OTA_H_*/
+/* PROTOTYPES *********************************************************************************/
+
+#endif /* OTA_H_ */
+
+/** @} */
